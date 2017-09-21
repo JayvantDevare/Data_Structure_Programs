34448d1416689fdf16825590a1922f5d33178fc1
JayvantDevare <jayvant.devare@gmail.com>
2017-09-21 13:45:15 +0530

    sample program in cpp

---
+++ sample_program.cpp
@@ -0,0 +1,9 @@
+#include "iostream"			//Header File Declaration
+using namespace std;
+int main()					//Main Function
+{
+	cout<<"\n Welcome to Data Structures World!!!!";  // printing on console
+	
+	return 0;				//returns value 0
+	
+}// closing of main function
