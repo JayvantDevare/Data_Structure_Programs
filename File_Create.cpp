e8a40ad07af0727686f48fa4010291ff6593a84f
JayvantDevare <jayvant.devare@gmail.com>
2017-09-22 09:56:21 +0530

    CPP Program for Creation of File

---
+++ File_Create.cpp
@@ -0,0 +1,25 @@
+// Program for creating a file in CPP
+
+#include <iostream>		//header file declaration
+#include <fstream>
+using namespace std;
+ 
+int main()					//main() function
+{
+   fstream file; 			//creating object of fstream class
+    
+  
+   file.open("myfile.txt",ios::out);  //opening file "mitaoe.txt" in write mode  (ios::out opens/create file in write mode)
+    
+   if(!file)				//checking for files
+   {
+       cout<<"Error in file Creation!!!!!!!";
+       return 0;
+   }
+    
+   cout<<"Congratulations File created successfully on your Computer";
+      
+   file.close(); //closing the file
+    
+   return 0;
+}//end of main() function
