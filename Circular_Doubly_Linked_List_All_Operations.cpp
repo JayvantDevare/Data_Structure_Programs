97bd06ab4eec77720a5c5161f131e2113412bbb6
Jayvant Devare <jayvant.devare@gmail.com>
2018-07-27 14:09:57 +0530

    Update Circular_Doubly_Linked_List_All_Operations.cpp

--- Circular_Doubly_Linked_List_All_Operations.cpp
+++ Circular_Doubly_Linked_List_All_Operations.cpp
@@ -1,29 +1,26 @@
 //Program for Circular Doubly Linked List in CPP
-#include<iostream>		//Header files
+#include<iostream>//Header files
 #include<cstdlib>
 using namespace std;
 int counter = 0;
-class Node					//class Node for creating node
+class Node//class Node for creating node
 {
-    public:
-			int data;
-		    Node *next;
-		    Node *prev;
+ public:
+ int data;
+ Node *next;
+ Node *prev;
 };//end Node class
-
-class Circular_Doubly_Linked_list			//class for Circular Doubly Linked List operations
+class Circular_Doubly_Linked_list//class for Circular Doubly Linked List operations
 {
     public:
     	Node* Head;
-    	Node* Last;
-    	
+    	Node* Last;    	
     	Circular_Doubly_Linked_list()
         {
             Head = NULL;
             Last = NULL;			
-        }
-        
-        Node *create_node();			//Circular Doubly Linked List all operations
+        }        
+        Node *create_node();//Circular Doubly Linked List all operations
         void insert_at_begin();
         void insert_at_last();
         void insert_at_position();
@@ -33,29 +30,25 @@ class Circular_Doubly_Linked_list			//class for Circular Doubly Linked List oper
         void reverseList();
         void delete_at_position();
         void displayData();
-};//end Circular Doubly Linked List class
- 
- //1. function for creating node for Circular Doubly Linked List
-
+};//end Circular Doubly Linked List class 
+//1. function for creating node for Circular Doubly Linked List
 Node* Circular_Doubly_Linked_list::create_node()
 {
     counter++;  
-    Node* newnode = new Node();		//creating node
-    cout<<"Enter the data: ";
-	cin>>newnode->data;
+    Node* newnode = new Node();//creating node
+    cout<<"Enter the data:";
+    cin>>newnode->data;
     newnode->next = NULL;
     newnode->prev = NULL;
     return newnode;
 }//end of create_node() function
-
- //2. function for inserting node at begining of the Circular Doubly Linked List
-
+//2. function for inserting node at begining of the Circular Doubly Linked List
 void Circular_Doubly_Linked_list::insert_at_begin()
 {
     int value;
     Node* newnode;
     newnode = create_node();
-    if (Head == Last && Head == NULL)		//checking for first location
+    if (Head == Last && Head == NULL)//checking for first location
     {    
         Head = Last = newnode;
         Head->next = Last->next = NULL;
@@ -68,7 +61,7 @@ void Circular_Doubly_Linked_list::insert_at_begin()
         Head = newnode;
         Head->prev = Last;
         Last->next = Head;
-        cout<<newnode->data<<" inserted in to the list"<<endl;
+        cout<<newnode->data<<"inserted in to the list"<<endl;
     }
 }//end of insert_at_begin() function
 
@@ -93,18 +86,16 @@ void Circular_Doubly_Linked_list::insert_at_last()
         Last->next = Head;
     }
 }//end of insert_at_Last() function
-
- //4. function for inserting node at given position of the Circular Doubly Linked List
-
+//4. function for inserting node at given position of the Circular Doubly Linked List
 void Circular_Doubly_Linked_list::insert_at_position()
 {    
     int position=0;
-	int i;
-    cout<<endl<<"Enter the postion where data to be inserted in to the list : ";
+    int i;
+    cout<<endl<<"Enter the postion where data to be inserted in to the list :";
     cin>>position;
     Node* newnode;
-	Node* trav;
-	Node* temp;
+    Node* trav;
+    Node* temp;
     newnode = create_node();
     if (Head == Last && Head == NULL)
     {
@@ -140,20 +131,18 @@ void Circular_Doubly_Linked_list::insert_at_position()
                 newnode->prev = temp;
                 newnode->next = trav;
                 trav->prev = newnode;
-                cout<<newnode->data<<" Element inserted at given position"<<position<<endl;
+                cout<<newnode->data<<"Element inserted at given position"<<position<<endl;
                 break;
             }
         }
     }
 }//end of insert_at_position() function
-
 //5. function for searching data from the  Circular Doubly Linked list
-
 void Circular_Doubly_Linked_list::searchData()
 {
     int position = 0;
-	int value;
-	int i;
+    int value;
+    int i;
     int flag = 0;
     Node* trav;
     if (Head == Last && Head == NULL)
@@ -161,7 +150,7 @@ void Circular_Doubly_Linked_list::searchData()
         cout<<"Sorry List is empty!!!!!"<<endl;
         return;
     }
-    cout<<endl<<"Enter the data to be searched	: ";
+    cout<<endl<<"Enter the data to be searched	:";
     cin>>value;
     trav = Head;
     for (i = 0;i < counter;i++)
@@ -169,31 +158,28 @@ void Circular_Doubly_Linked_list::searchData()
         position++;
         if (trav->data == value)
         {
-            cout<<"Data "<<value<<" found in the list at position	: "<<position<<endl;
+            cout<<"Data "<<value<<" found in the list at position	:"<<position<<endl;
             flag = 1;
         }    
         trav = trav->next;
     }
     if (!flag)
-        cout<<"Data not found in the list"<<endl;   
+      cout<<"Data not found in the list"<<endl;   
 }//end of searchData() function
-
 //6. function for updating data into the Circular Doubly Linked list
-
 void Circular_Doubly_Linked_list::updateData()
 {
     int value;
-	int position;
-	int i;
-	
+    int position;
+    int i;	
     if (Head == Last && Head == NULL)
     {
         cout<<"Sorry List is empty"<<endl;
         return; 
     }
-    cout<<endl<<"Enter the postion of node to be updated	: ";
+    cout<<endl<<"Enter the postion of node to be updated	:";
     cin>>position;
-    cout<<"Enter the new value for updation : ";
+    cout<<"Enter the new value for updation :";
     cin>>value;
     Node* trav;
     if (counter < position)
@@ -205,7 +191,7 @@ void Circular_Doubly_Linked_list::updateData()
     if (position == 1)
     {
        trav->data = value;
-       cout<<"data Updated at :	"<<position<<endl;
+       cout<<"data Updated at :"<<position<<endl;
        return;   
     }
     for (i=0;i < position - 1;i++)
@@ -213,15 +199,13 @@ void Circular_Doubly_Linked_list::updateData()
         trav = trav->next; 		 
     }
     trav->data = value;
-    cout<<"Data Updated at :	"<<position<<endl;
+    cout<<"Data Updated at :"<<position<<endl;
 }//end of updateData() function
-
- //7. function for sorting data of the Circular Doubly Linked list
-
+//7. function for sorting data of the Circular Doubly Linked list
 void Circular_Doubly_Linked_list::sortData()
 {
     Node* temp;
-	Node* trav;
+    Node* trav;
     int i;
     int value;
     if (Head == Last && Head == NULL)
@@ -246,9 +230,7 @@ void Circular_Doubly_Linked_list::sortData()
         trav = trav->next;
     }
 }//end of sortDdata() function
-
- //8. function for Reversing data of the Circular Doubly Linked list
-
+//8. function for Reversing data of the Circular Doubly Linked list
 void Circular_Doubly_Linked_list::reverseList()
 {
     if (Head == Last && Head == NULL)
@@ -257,11 +239,10 @@ void Circular_Doubly_Linked_list::reverseList()
         return;
     }
     Node* trav;
-	Node* temp = Head;
+    Node* temp = Head;
     trav = temp->next;
     temp->next = NULL;
-    temp->prev = trav;
-    
+    temp->prev = trav;    
     while (trav != Head)
     {
         trav->prev = trav->next;
@@ -272,21 +253,19 @@ void Circular_Doubly_Linked_list::reverseList()
     Last = Head;
     Head = temp;
 }//end of reverseList() function
-
- //9. function for deleting data by position from the Circular Doubly Linked list
-
+//9. function for deleting data by position from the Circular Doubly Linked list
 void Circular_Doubly_Linked_list::delete_at_position()
 {    
     int position;
-	int i;
+    int i;
     Node* temp;
-	Node* trav;
+    Node* trav;
     if (Head == Last && Head == NULL)
     {
         cout<<"Sorry list is empty"<<endl;
         return;
     }
-    cout<<endl<<"Enter the postion of node to be deleted: ";
+    cout<<endl<<"Enter the postion of node to be deleted:";
     cin>>position;
     if (counter < position)
     {
@@ -300,9 +279,8 @@ void Circular_Doubly_Linked_list::delete_at_position()
         Last->next = trav->next;
         trav->next->prev = Last;
         Head = trav->next;
-        cout<<trav->data<<"Element Deleted at :	"<<position<<endl;
-        free(trav);
-       
+        cout<<trav->data<<"Element Deleted at :"<<position<<endl;
+        free(trav);       
         return;	   
     }
     for (i = 0;i < position - 1;i++ )
@@ -317,12 +295,10 @@ void Circular_Doubly_Linked_list::delete_at_position()
         Last = temp; 	   
     }
     counter--;
-    cout<<trav->data<<"Element Deleted at :	"<<position<<endl;
+    cout<<trav->data<<"Element Deleted at :"<<position<<endl;
     free(trav);
 }// end of delete_at_position() function
-
- //10. function for displaying data  from the Circular Doubly Linked list
-
+//10. function for displaying data  from the Circular Doubly Linked list
 void Circular_Doubly_Linked_list::displayData()
 {
     int i;
@@ -340,14 +316,10 @@ void Circular_Doubly_Linked_list::displayData()
     }
     cout<<trav->data<<endl;
 }//end of displayData() function
-
-
-int main()		//main() function
+int main()//main() function
 {
-    int choice;
-    
-    Circular_Doubly_Linked_list cdl; //creating object of Circular_Doubly_Linked_list class
-    
+    int choice;    
+    Circular_Doubly_Linked_list cdl;//creating object of Circular_Doubly_Linked_list class    
     do
     {
         cout<<"\n-------------------------------"<<endl;
@@ -363,27 +335,26 @@ int main()		//main() function
         cout<<"8.Delete data  from given Position"<<endl;
         cout<<"9.Display Doubly Circular Linked List"<<endl;
         cout<<"10.Exit"<<endl;
-        cout<<"Enter your choice : ";
-        cin>>choice;
-        
+        cout<<"Enter your choice :";
+	cin>>choice;        
         switch(choice)
         {
         case 1:
-	        	cout<<"\n Inserting data at begining of the List"<<endl;
-	            cdl.insert_at_begin();
-	            break;
+	       cout<<"\nInserting data at begining of the List"<<endl;
+	       cdl.insert_at_begin();
+	       break;
         case 2:
-	        	cout<<"\n Inserting data at Last of the List"<<endl;
-	            cdl.insert_at_last();
-	            break;   
+	       cout<<"\nInserting data at Last of the List"<<endl;
+	       cdl.insert_at_last();
+	       break;   
         case 3:
-	        	cout<<"\n Inserting data at given position in the List"<<endl;
-	            cdl.insert_at_position();
-	            break; 
+	       cout<<"\nInserting data at given position in the List"<<endl;
+	       cdl.insert_at_position();
+	       break; 
         case 4:
-	        	cout<<"\n Searching data in the List"<<endl;
-	            cdl.searchData();
-	            break;
+	       cout<<"\nSearching data in the List"<<endl;
+	       cdl.searchData();
+	       break;
         case 5:
 	        	cout<<"\n Updating data in the List"<<endl;
 	            cdl.updateData();
@@ -391,37 +362,34 @@ int main()		//main() function
 	            cdl.displayData();
 	            break;
         case 6:
-        		cout<<"\n Before sorting List"<<endl;
-				cdl.displayData();
-	        	cout<<"\n Sorting data of the List"<<endl;
-	            cdl.sortData();
-	            cout<<"\n After sorting List"<<endl;
-	            cdl.displayData();
-	            break;
+        	cout<<"\nBefore sorting List"<<endl;
+		cdl.displayData();
+	        cout<<"\nSorting data of the List"<<endl;
+	        cdl.sortData();
+	        cout<<"\nAfter sorting List"<<endl;
+	        cdl.displayData();
+	        break;
         case 7:
-	        	cout<<"\n Reversing List"<<endl;
-	            cdl.reverseList();
-	            cout<<"\n After Reversing the List"<<endl;
-	            cdl.displayData();
-	            break;
+	       cout<<"\nReversing List"<<endl;
+	       cdl.reverseList();
+	       cout<<"\nAfter Reversing the List"<<endl;
+	       cdl.displayData();
+	       break;
         case 8:
-	        	cout<<"\n Deleting data from given position"<<endl;
-	            cdl.delete_at_position();
-	            cout<<"\n after node deletion "<<endl;
-	            cdl.displayData();
-	            break;
+	       cout<<"\nDeleting data from given position"<<endl;
+	       cdl.delete_at_position();
+	       cout<<"\nAfter node deletion "<<endl;
+	       cdl.displayData();
+	       break;
         case 9:
-	        	cout<<"\n Displaying data from the List"<<endl;
-	            cdl.displayData();
-	            break;
+	       cout<<"\nDisplaying data from the List"<<endl;
+	       cdl.displayData();
+	       break;
         case 10:
-           		exit(1); 
+               exit(1); 
         default:
             	cout<<"Wrong choice"<<endl;	
-        }
-        
-    }while(choice!=10);
-    
+        }        
+    }while(choice!=10);    
     return 0;
 }//end of main() function
-
