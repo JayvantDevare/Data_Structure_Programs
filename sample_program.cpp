f3e10480f769ebb16142616be8ed632001c8975b
Jayvant Devare <jayvant.devare@gmail.com>
2018-07-27 14:00:33 +0530

    Update sample_program.cpp

--- sample_program.cpp
+++ sample_program.cpp
@@ -1,9 +1,7 @@
-#include "iostream"			//Header File Declaration
+#include "iostream"//Header File Declaration
 using namespace std;
-int main()					//Main Function
+int main()//Main Function
 {
-	cout<<"\n Welcome to Data Structures World!!!!";  // printing on console
-	
-	return 0;				//returns value 0
-	
+cout<<"\n Welcome to Data Structures World!!!!"; // printing on console	
+return 0;//returns value 0	
 }// closing of main function
